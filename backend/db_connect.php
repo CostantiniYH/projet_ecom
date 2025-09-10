09c9a354822a34a275f8b2315d39bccb8a9689ae
CostantiniYH <costantiniyaacov@gamil.com>
2025-09-10 21:51:41 +0200

    auto deploy

--- backend/db_connect.php
+++ backend/db_connect.php
@@ -16,7 +16,7 @@ function connect () {
     try {
 
         $dsn = "mysql:host=localhost;dbname=yhc";
-        $user = "YHC";
+        $user = "root";
         $passwd = "";
 
         $pdo = new PDO($dsn, $user, $passwd);
