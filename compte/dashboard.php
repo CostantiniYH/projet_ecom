f285de2e8a30087ffd0f6853e7472c0fd01e8f11
CostantiniYH <costantiniyaacov@gamil.com>
2025-09-14 01:03:15 +0200

    auto deploy

--- compte/dashboard.php
+++ compte/dashboard.php
@@ -5,13 +5,16 @@ require_once __DIR__ . '/../class/navbar.php';
 
 require_login();
 
-$connect = connect();
+$pdo = connect();
 
 if (!isset($_SESSION['user'])) {
     die("Erreur : utilisateur non connecté.");
 } 
 
 $user = $_SESSION['user'];
+$id = $_SESSION['user']['id'];
+$myProduits = findBy($pdo, 't_produits', 'id_user', $id);
+$myImage = findBy($pdo, 't_images', 'id_user', $id);
 
 $navbar = new Navbar();
     $navbar->AddItem('|| YHC ||','index.php', 'left', '', '');
@@ -38,34 +41,98 @@ $navbar->render();
     <h1 class="shadow rounded p-4">Dashboard <?= $user['nom']; ?> <?= $user['prenom']; ?></h1>
 
     <img class="bandeau rounded-4 shadow" src="<?= BASE_URL . $user['photo']; ?>">
-    <div class="d-flex row mt-4 shadow rounded border border-1 border-success table-responsive">
-        <table class="table row mt-2 shadow col-md-4 table-responsive">
-            <h3>Vos informations</h3>
-            <tr class="">
-                <th>Nom</th>
-                <td> <?= $user['nom']?> </td>
-            </tr>
+    <div class="row mt-5 gap-5">
+        <div class="col-md table-responsive">
+            <div class="p-3 shadow rounded border border-1 border-success mb-5">
+            <table class="table mt-2">
+                <h3>Mes informations</h3>
+                <tr class="">
+                    <th>Nom</th>
+                    <td> <?= $user['nom']?> </td>
+                </tr>
+                <tr>
+                    <th>Prénom</th>
+                    <td> <?= $user['prenom']?> </td>
+                </tr>
+                <tr>
+                    <th class="">Email</th>
+                    <td> <?= $user['email']; ?> </td>
+                </tr>
+                <tr>    
+                    <th class="">Téléphone</t>
+                    <td> <?= $user['telephone']; ?> </td>
+                </th>
+                <tr>
+                    <th class="">Société</th>  
+                    <td> <?= $user['societe']; ?> </td>
+                </tr>
+                <tr>
+                    <th>Photo profil</th>              
+                    <td> <img width="100" class="rounded" src="<?= BASE_URL . $user['photo']; ?>"> </td>
+                </tr>
+            </table>
+            </div>
+             <div class="p-3 shadow rounded border border-1 border-success mt-5">
+            <table class="table mt-2">
+                <h3>Mes images</h3>
+                <?php 
+                if (!empty($myImage[0]) && isset($image[0])) {
+                    $fields = array_keys($myImage[0]);
+                    foreach ($fields as $field) {
+                        $label = preg_replace('/[^a-zA-Z0-9]/', ' ', $field);
+                ?>                
+                <tr>
+                    <th><?= ucfirst($label)?></th>
+                    <?php foreach ($myProduits as $mP) { ?>
+                    <td>
+                        <?php if ($field === 'image') { ?>
+                            <img width="100" src="<?= BASE_URL . $mP[$field] ?>">
+                        <?php } else { ?>
+                            <?= $mP[$field]; ?>
+                        <?php } ?>
+                    </td>
+                    <?php } ?>
+                </tr>
+                <?php } 
+            } else { ?>
             <tr>
-                <th>Prénom</th>
-                <td> <?= $user['prenom']?> </td>
+                <td><div class="alert alert-warning alert-dismissible fade show" data-bs-dismiss="3000" role="alert">Vous n'avez pas encore ajouté d'image</div></td>
             </tr>
-            <tr>
-                <th class="">Email</th>
-                <td> <?= $user['email']; ?> </td>
-            </tr>
-            <tr>    
-                <th class="">Téléphone</t>
-                <td> <?= $user['telephone']; ?> </td>
-            </th>
-            <tr>
-                <th class="">Société</th>  
-                <td> <?= $user['societe']; ?> </td>
-            </tr>
-            <tr>
-                <th>Photo profil</th>              
-                <td> <img width="100" class="rounded" src="<?= BASE_URL . $user['photo']; ?>"> </td>
-            </tr>
-        </table>
+            <?php } ?>
+            </table>
+            </div>
+        </div>
+        <div class="p-3 col-md flex-end shadow rounded border border-1 border-success table-responsive">
+            <table class="table">
+                <h3>Mes produits</h3>
+                <?php
+                if (!empty($myProduits) && isset($myProduits)) {
+                    $fields = array_keys($myProduits[0]);
+                    foreach ($fields as $field) {
+                        $label = preg_replace('/[^a-zA-Z0-9]/', ' ', $field);
+                ?>                
+                <tr>
+                    <th><?= ucfirst($label)?></th>
+                    <?php foreach ($myProduits as $mP) { ?>
+                    <td>
+                        <?php if ($field === 'image') { ?>
+                            <img width="100" src="<?= BASE_URL . $mP[$field] ?>">
+                        <?php } else { ?>
+                            <?= $mP[$field]; ?>
+                        <?php } ?>
+                    </td>
+                    <?php } ?>
+                </tr>
+                <?php }
+                } else { ?>
+                    <tr>
+                        <td>
+                           <div class="alert alert-warning alert-dismissible fade show" data-bs-dismiss="3000" role="alert">Vous n'avez pas encore ajouté de produit</div>
+                        </td>
+                    </tr>
+                    <?php } ?>
+            </table>
+        </div>
     </div>
 </div>
 <?php 
