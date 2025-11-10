ec6d2e1a2e1459f1c1f411ddef445b39994a26ea
CostantiniYH <costantiniyaacov@gamil.com>
2025-11-10 18:16:52 +0100

    en cours...

--- app/Controllers/AuthController.php
+++ app/Controllers/AuthController.php
@@ -1,10 +1,10 @@
 <?php
 namespace App\Controllers;
-
-
 use App\Config\Database;
 use PDO;
 use Exception;
+use App\Classes\User;
+
 
 class AuthController {
     public function login() {
@@ -17,6 +17,42 @@ class AuthController {
         $content = ob_get_clean();
 
         require_once __DIR__ . '/../View/partials/layout.php';
+
+        //require_once __DIR__ . '/../../controllers/session.php';
+        //require_once __DIR__ . '/../../class/user.php';
+
+
+        if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+            http_response_code(405);
+            exit;
+        }
+
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+
+        $pdo = Database::connect();
+
+        $value = findBy2( $pdo, '*', 't_users', 'email', $email);
+
+        if (is_array($value) && count($value) == 0) {
+            header('Location: ' . BASE_URL . 'Form/Compte/login.php?erreur=L\'utilisateur n\'existe pas.');
+            exit();
+        }
+
+        $password_hash = $value['password'];
+
+        if (User::verifyPassword($password, $password_hash)) {
+            loginUser($value);
+            if (isAdmin()) {
+                header('Location: ' . BASE_URL . 'admin/dashboard.php?success=Vous êtes connecté en tant qu\'administrateur.');
+                exit();
+            } 
+            header ('Location: ' . BASE_URL . 'compte/dashboard.php?success=Connexion réussi !');
+            exit();
+        } else {
+            header('Location: ' . BASE_URL . 'Form/Compte/login.php?erreur=Le mot de passe est incorrecte !');
+            exit();
+        }
     }
 
     public function register() {
