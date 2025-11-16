59d59acf56253e6b0081081aa5cbe843211221c1
CostantiniYH <costantiniyaacov@gamil.com>
2025-11-16 16:40:07 +0100

    dvdfvds

--- app/init.php
+++ app/init.php
@@ -1,8 +1,7 @@
 <?php
-
 require_once __DIR__ . '/core/session.php';
 require_once __DIR__ . '/Models/user.php';
-require_once __DIR__ . '/core/navbarBuilder.php';
+require __DIR__ . '/core/navbarBuilder.php';
 
 spl_autoload_register(function ($class) {
     $prefix = 'App\\';
