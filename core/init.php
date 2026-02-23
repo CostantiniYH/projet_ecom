90066315b17fb0b3dab6fca7843a67db11acad90
CostantiniYH <costantiniyaacov@gmail.com>
2026-02-23 22:34:45 +0100

    namaesace Produit, Carousel, Navbar

--- core/init.php
+++ core/init.php
@@ -1,20 +1,21 @@
 <?php
+use App\Config\Database;
+use Core\Routing\Router;
+
 require dirname(__DIR__) . '/app/Config/config.php';
 require dirname(__DIR__) . '/app/Views/partials/navbarBuilder.php'; 
 require dirname(__DIR__) . '/app/coreTemp/session.php';
 require dirname(__DIR__) . '/app/Models/Requetes/ProduitModel.php';
 
-
-use App\Config\Database;
-$pdo = Database::connect();
-
-use Core\Routing\Router;
-$router = new Router($pdo);
-$router->run();
-
 require_once dirname(__DIR__) . '/routes/web.php';
 require_once dirname(__DIR__) . '/routes/auth.php';
 require_once dirname(__DIR__) . '/routes/user.php';
 require_once dirname(__DIR__) . '/routes/admin.php';
+
+$pdo = Database::connect();
+
+$router = new Router($pdo);
+$router->run();
+
 
 
